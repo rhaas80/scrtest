73e3a8912df75a2a82e1a68dc463f1658af3552e
Roland Haas <haas12@catalyst160.llnl.gov>
2020-04-22 06:49:51 -0700

    add some test to trigger failures during checkpoint and run

--- scrtest.c
+++ scrtest.c
@@ -1,21 +1,34 @@
 #include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <errno.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <mpi.h>
 
 #include "scr.h"
+// internal SCR function to get remaining job time
+extern long int scr_env_seconds_remaining(void);
 
-#define ITER_INC 10 // how many iterations before we terminate voluntarily
+#define SCR_HALT_SECONDS 120
+
 int counter;       // my "state", just the iteration counter offset by rank
+int restartcount;  // how often we have restarted
 const char *prefix;
 
-void checkpoint(int rank)
+enum failure_t {NO_FAIL, FAIL, FAIL_LATE}; /* should we trigger an intentional failure? */
+
+void checkpoint(int rank, enum failure_t do_fail)
 {
   // inform SCR that we are starting a new checkpoint
   SCR_Start_checkpoint();
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(do_fail == FAIL)
+    exit(1);
 
   // build the filename for our checkpoint file
   char buf[SCR_MAX_FILENAME];
@@ -31,18 +44,29 @@ void checkpoint(int rank)
   int valid = 1;
   FILE *fh = fopen(scr_file, "w");
   assert(fh);
-  fprintf(fh, "%d\n", counter);
+  fprintf(fh, "counter %d\n", counter);
+  fprintf(fh, "rank %d\n", rank);
   fclose(fh);
 
   // inform SCR whether this process wrote each of its
   // checkpoint files successfully
   SCR_Complete_checkpoint(valid);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(do_fail == FAIL_LATE) {
+    remove(scr_file); /* simulate node failure by removing checkpoint */
+    exit(1);
+  }
 }
 
-void restart(int rank)
+void restart(int rank, enum failure_t do_fail)
 {
   // inform SCR that we are starting a recovery
   SCR_Start_restart(NULL);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(do_fail == FAIL)
+    exit(1);
 
   // build the filename for our checkpoint file
   char buf[SCR_MAX_FILENAME];
@@ -57,12 +81,61 @@ void restart(int rank)
   int valid = 1;
   FILE *fh = fopen(scr_file, "r");
   assert(fh);
-  fscanf(fh, "%d", &counter);
+  fscanf(fh, "counter %d", &counter);
+  int cprank;
+  fscanf(fh, "rank %d\n", &cprank);
+  if(cprank != rank) {
+    fprintf(stderr, "Unexpected rank value in checkpoint file: %d != %d\n", cprank, rank);
+    exit(1);
+  }
   fclose(fh);
 
   // inform SCR whether this process read each of its
   // checkpoint files successfully
   SCR_Complete_restart(valid);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(do_fail == FAIL_LATE)
+    exit(1);
+}
+
+long current_time_on_rank0(int rank)
+{
+  long retval;
+
+  if(rank == 0) {
+    time_t now = time(NULL);
+    retval = (long)now;
+  }
+  MPI_Bcast(&retval, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+
+  return retval;
+}
+
+void read_restart(int rank)
+{
+  const char *fn = "restartcount.dat";
+  FILE *fh = fopen(fn, "r");
+  if(fh != NULL) {
+    fscanf(fh, "restartcount %d", &restartcount);
+    fclose(fh);
+  } else {
+    restartcount = 0;
+  }
+
+  restartcount += 1;
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  if(rank == 0) {
+    FILE *fh = fopen(fn, "w");
+    if(fh == NULL) {
+      fprintf(stderr, "Failed to open '%s': %s", fn, strerror(errno));
+      exit(1);
+    }
+    fprintf(fh, "restartcount %d", restartcount);
+    fclose(fh);
+  }
 }
 
 int main(int argc, char **argv)
@@ -75,51 +148,94 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &ranks);
 
+  long start_time = current_time_on_rank0(rank);
+
   // disabled buffering for progress output
   setbuf(stdout, NULL);
 
   if (rank == 0)
     printf("Starting...\n");
 
+  // find out which step in the test scheme we are at. Do not use SCR
+  // checkpoints for this so that we can test recovery failures.
+  read_restart(rank);
+
   // this can only be set via ENV vars since the Perl scripts only look at them
   if ((prefix = getenv("SCR_PREFIX")) == NULL)
     prefix = ".";
 
   // set some SCR options
-  const char *cfg1 = SCR_Config("SCR_CHECKPOINT_SECONDS=3");
+  const char *cfg1 = SCR_Config("SCR_CHECKPOINT_SECONDS=30");
   assert(cfg1);
-  const char *cfg2 = SCR_Config("SCR_COPY_TYPE=SINGLE");
+  char cfg2buf[100];
+  snprintf(cfg2buf, sizeof(cfg2buf), "SCR_HALT_SECONDS=%d", SCR_HALT_SECONDS);
+  const char *cfg2 = SCR_Config(cfg2buf);
   assert(cfg2);
 
   if(SCR_Init() == SCR_SUCCESS) {
     // initialization
     int have_restart;
     SCR_Have_restart(&have_restart, NULL);
-    if (have_restart)
-      restart(rank);
-    else
-      counter = rank;
+    if (have_restart) {
+      /* TODO: figure out how to simulate restarting failing */
+      restart(rank, NO_FAIL);
+    } else {
+      counter = 0;
+    }
 
     // main loop
-    int initial_counter = counter;
     if (rank == 0)
-      printf("Iterating from %d for %d iterations\n", initial_counter, ITER_INC);
-    while(counter - initial_counter < ITER_INC) {
+      printf("Starging from %d\n", counter);
+    while(1) {
       if (rank == 0)
         printf("Checking consistency...\n");
       // check consistency of data across ranks
       int global_counter = counter;
       MPI_Bcast(&global_counter, 1, MPI_INT, 0, MPI_COMM_WORLD);
-      if(global_counter + rank != counter) {
-        fprintf(stderr, "Inconsistent state %d != %d on rank %d\n", global_counter + rank, counter, rank);
+      if(global_counter != counter) {
+        fprintf(stderr, "Inconsistent state %d != %d on rank %d\n", global_counter, counter, rank);
         MPI_Abort(MPI_COMM_WORLD, 1);
       }
 
+      long now = current_time_on_rank0(rank);
       if (rank == 0)
-        printf("In iteration %d\n", counter);
+        printf("In iteration %d, running %ld seconds\n", counter, now - start_time);
       // "science" loop
-      sleep(1);
+      sleep(10);
       counter += 1;
+
+      // trigger various issues
+      MPI_Barrier(MPI_COMM_WORLD);
+      if(counter == 1 && restartcount == 0) {
+        // get some initial checkpoint
+        checkpoint(rank, NO_FAIL);
+      }
+      if(rank == 0 && counter == 2 && restartcount == 0) {
+        // trigger a failure so that we recover from checkpoint just written
+        exit(1);
+      }
+      if(counter == 3 && restartcount == 1) {
+        // trigger a failure while writing a checkpoint
+        if(rank == 1) {
+          checkpoint(rank, FAIL);
+        } else {
+          checkpoint(rank, NO_FAIL);
+        }
+      }
+      if(counter == 4 && restartcount == 2) {
+        // try simulating failure by first writing a checkpoint then wiping it
+        if(rank == 1) {
+          checkpoint(rank, FAIL_LATE);
+        } else {
+          checkpoint(rank, NO_FAIL);
+        }
+      }
+      if(counter == 5 && restartcount == 3) {
+        // cause a failure later enough so that SCR should not restart
+        sleep(scr_env_seconds_remaining() - SCR_HALT_SECONDS + 30);
+        exit(1);
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
 
       // ask SCR whether we need to checkpoint
       int flag = 0;
@@ -128,7 +244,8 @@ int main(int argc, char **argv)
         if (rank == 0)
           printf("Checkpointing at iteration: %d\n", counter);
         // execute the checkpoint code
-        checkpoint(rank);
+        checkpoint(rank, NO_FAIL);
+        MPI_Barrier(MPI_COMM_WORLD);
       }
 
       // should we exit?
@@ -140,7 +257,7 @@ int main(int argc, char **argv)
     printf("Done with iterations\n");
 
     // termination checkpoint
-    checkpoint(rank);
+    checkpoint(rank, NO_FAIL);
 
     SCR_Finalize();
 
